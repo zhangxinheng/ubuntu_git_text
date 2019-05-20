a0400c5544e436fa4bdc8cec1b40f2687f9f1611
ScrapyCrawl <13485966130@163.com>
2019-05-21 00:35:50 +0800

    four

--- untitled2/IndexMaxHeap.h
+++ untitled2/IndexMaxHeap.h
@@ -8,15 +8,18 @@
 #include <stdlib.h>
 using namespace std;
 template <typename Item>
-class IndexMaxHeap{
+class IndexMaxHeap{//索引堆
 private:
     Item*data;
     int* indexs;
+    int*reverse;
     int count;
     int capacity;
     void shiftUp(int k){
         while(k>1&&data[indexs[k/2]]<data[indexs[k]]){
             swap(indexs[k/2],indexs[k]);
+            reverse[indexs[k/2]]=k/2;
+            reverse[indexs[k]]=k;
             k/=2;
         }
     }
@@ -30,6 +33,8 @@ private:
                 break;
             }
             swap(indexs[k],indexs[j]);
+            reverse[indexs[k]]=k;
+            reverse[indexs[j]]=j;
             k=j;
         }
     }
@@ -38,6 +43,10 @@ public:
         data=new Item[capacity];
         count=0;
         indexs=new int[capacity+1];
+        reverse=new int[capacity+1];
+        for(int i=0;i<=capacity;i++){
+            reverse[i]=0;
+        }
         this->capacity=capacity;
     }
     IndexMaxHeap(Item arr[],int n){
@@ -54,6 +63,7 @@ public:
     ~IndexMaxHeap(){
         delete [] data;
         delete [] indexs;
+        delete [] reverse;
     }
     int size(){
         return count;
@@ -66,6 +76,7 @@ public:
         assert(i+1>=1&&i+1<=capacity);
         data[i]=item;
         indexs[count+1]=i;
+        reverse[i]=count+1;
         count++;
         shiftUp(count);
     }
@@ -79,6 +90,8 @@ public:
         assert(count>0);
         Item ret=data[indexs[1]];
         swap(indexs[1],indexs[count]);
+        reverse[indexs[1]]=1;
+        reverse[indexs[count]]=0;//讲该元素删除就是0
         count--;
         shiftDown(1);
         return ret;
@@ -88,24 +101,36 @@ public:
         assert(count>0);
         int ret=indexs[1]-1;//对于用户来说索引是从0开始的
         swap(indexs[1],indexs[count]);
+        reverse[indexs[1]]=1;
+        reverse[indexs[count]]=0;//讲该元素删除就是0
         count--;
         shiftDown(1);
         return ret;
     }
+    bool contain(int i){
+        assert(i+1>=1&&i+1<=capacity);
+        return reverse[i+1]!=0;
+    }
     Item getItem(int i){
+        assert(contain(i));
         return data[i+1];
     }
 
-    void change(int i,Item newItem){
+    void change(int i,Item newItem){//修改元素
+        assert(contain(i));//判断索引堆中是否包含i
         i+=1;
         data[i]=new Item;
-        for(int j=1;j<+count;j++){
-            if(indexs[j]==i){
-                shiftUp(j);
-                shiftDown(j);
-                return;;
-            }
-        }
+//        for(int j=1;j<=count;j++){
+//            if(indexs[j]==i){
+//                shiftUp(j);
+//                shiftDown(j);
+//                return;;
+//            }
+//        }
+//找到indexes[j]=i,j表示data[i]在堆中的位置。
+    int j=reverse[i];
+    shiftDown(j);
+    shiftUp(j);
     }
 
 
